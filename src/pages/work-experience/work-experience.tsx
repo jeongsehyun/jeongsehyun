07422e5c679004cd1ec535323967d555707c10d4
sehyun.dev <sehyun.dev@gmail.com>
2022-02-03 17:28:46 +0900

    Add workExperience page

--- src/pages/work-experience/work-experience.tsx
+++ src/pages/work-experience/work-experience.tsx
@@ -1,8 +1,25 @@
+import PageContainer from "components/page-container/page-container";
 import React from "react";
 import styles from "./work-experience.module.css";
 
 function WorkExperience() {
-  return <div className="page"></div>;
+  return (
+    <PageContainer title="WorkExperience">
+      <div className={styles.wrap}>
+        <div className={styles.company}>
+          <h2 className={styles.companyName}>Radiuslab</h2>
+          <p className={styles.date}>(2020.08 ~ 2021.10)</p>
+        </div>
+        <div className={styles.contents}>
+          <p>커뮤니티 차량공유 서비스 WeGooli 앱 개발/배포</p>
+          <ul className={styles.list}>
+            <li>React Native</li>
+            <li>예약, 결제, 마이페이지 화면 및 기능 구현</li>
+          </ul>
+        </div>
+      </div>
+    </PageContainer>
+  );
 }
 
 export default WorkExperience;
